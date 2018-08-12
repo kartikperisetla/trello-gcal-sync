b497f2cdbea6f608077d0834de4978080b56189a
Kartik Perisetla <kartik.peri@gmail.com>
2018-08-12 08:01:17 -0700

    adding settings file for configurable items; board manager to fetch board and card details

---
+++ lib/board_manager.py
@@ -0,0 +1,50 @@
+import requests           
+import json
+from auth_mgr import *
+from board import *
+from settings import *
+
+class BoardManager:
+    def __init__(self, auth_mgr=None):
+        self._url = "https://trello.com/1/members/me/boards"
+        self.args = {'fields': 'name', 'lists': 'open'}
+        self.auth_mgr = auth_mgr
+
+    def _check_valid_auth_mgr(self):
+        if self.auth_mgr is None:
+            print(self.__class__.__name__+":Invalid Auth manager- try passing valid auth manager.")
+            exit()
+
+    def get_boards(self):
+        self._check_valid_auth_mgr()
+
+        response = requests.get(self._url, params=self.auth_mgr.get_key_token(), data=self.args)
+
+        if response.status_code == 200:
+            board_coll = self._parse_json_for_boards(response.json())
+            self._populate_boards_with_cards(board_coll)
+
+
+    def _parse_json_for_boards(self, json=None):
+        if json is None:
+            return
+        resp = []
+
+        for board in json:
+            new_b = Board(auth_mgr=self.auth_mgr)
+            for key, val in board.items():
+                new_b.add_prop(key, val)
+            resp.append(new_b)        
+        return resp
+
+    def _populate_boards_with_cards(self, board_coll):
+        for _board in board_coll:
+            _board.fetch_cards()
+
+        
+
+if __name__ == "__main__":
+    am = AuthManager(key = KEY, token = TOKEN)
+
+    bm = BoardManager(auth_mgr=am)
+    bm.get_boards()
