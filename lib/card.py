3a4b717fbffd350c106fe130b8ad274331add854
Kartik Perisetla <kartik.peri@gmail.com>
2018-09-04 03:54:01 -0700

    adding methods to get card hash; check if due date is in future

--- lib/card.py
+++ lib/card.py
@@ -1,6 +1,20 @@
-from base_entity import *
+from lib.base_entity import *
+from datetime import timezone
+from datetime import datetime
 
 class Card(BaseEntity):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.type = "CARD"
+    
+    # method that returns hash representing card name and due date
+    # <card_name:due_date>
+    def get_card_hash(self):
+        hash_val = self.get_prop("name") + ":" + self.get_prop("due")
+        return hash_val
+
+    def is_card_due_in_future(self):
+        due_date = self.get_prop("due")
+        due_date = datetime.strptime(due_date, '%Y-%m-%dT%H:%M:%S.%fz')
+        local_date = due_date.replace(tzinfo=timezone.utc).astimezone(tz=None)
+        print("local_date=",local_date," for utc_date=", self.get_prop("due"))
